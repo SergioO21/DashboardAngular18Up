4067081fce32cff065f878ae270cae190d941f81
Sergio Orejarena <sergioorueda@gmail.com>
2025-01-26 16:57:07 -0500

    Routes as menu options

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { routes } from "../../app.routes";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "side-menu",
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: "./side-menu.component.html",
   styles: ``,
 })
