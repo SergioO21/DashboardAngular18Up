0497697aaad3b3fa7ba8b8f02387e66837fcbd28
Sergio Orejarena <sergioorueda@gmail.com>
2025-01-26 21:49:49 -0500

    Create change detection component

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,9 +1,46 @@
-import { Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from "@angular/core";
+import { TitleComponent } from "@shared";
+import { CommonModule } from "@angular/common";
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: "./change-detection.component.html",
-  styles: ``,
+  imports: [TitleComponent, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
+  `,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  currentFramework = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  );
+
+  frameworkAsSignal = signal({
+    name: "Angular",
+    releaseDate: 2016,
+  });
+
+  frameworkAsProperty = {
+    name: "Angular",
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = "React";
+      this.frameworkAsSignal.update((framework) => ({
+        ...framework,
+        name: "React",
+      }));
+      console.log("Change");
+    }, 3000);
+  }
+}
