bd5ed4b41fa0b06e5899e85da7cfd4572f69f608
Sergio Orejarena <sergioorueda@gmail.com>
2025-01-26 21:30:28 -0500

    Create title component

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { booleanAttribute, Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-title",
   standalone: true,
   imports: [],
-  templateUrl: "./title.component.html",
+  template: `
+    <h1 class="text-3xl m-5" [class.shadow-2xs]="withShadow">
+      {{ title }}
+    </h1>
+  `,
   styles: ``,
 })
-export class TitleComponent {}
+export class TitleComponent {
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+}
