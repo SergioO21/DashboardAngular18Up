c6f7ab8cef605512f7d3021868551f730d2bf501
Sergio Orejarena <sergioorueda@gmail.com>
2025-01-26 21:30:55 -0500

    Create control flow component

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,9 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { TitleComponent } from "@shared";
+
+type Grade = "A" | "B" | "C";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: "./control-flow.component.html",
   styles: ``,
 })
-export default class ControlFlowComponent {}
+export default class ControlFlowComponent {
+  showContent = signal(false);
+  grade = signal<Grade>("A");
+  frameworks = signal(["Angular", "Vue", "Svelte", "Qwik", "React"]);
+  frameworksEmpty = signal([]);
+
+  toggleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
